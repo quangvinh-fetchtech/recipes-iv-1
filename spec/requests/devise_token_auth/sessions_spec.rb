6578bb6c3c6b8f5e56e35ebd2fc7f854d4b50c45
Vinh Nguyen <vinh.nguyen@coinhako.com>
2022-08-15 19:46:30 +0700

    fix

--- spec/requests/devise_token_auth/sessions_spec.rb
+++ spec/requests/devise_token_auth/sessions_spec.rb
@@ -4,20 +4,6 @@ RSpec.describe 'devise_token_auth/sessions', type: :request do
 
   path '/api/v1/auth/sign_in' do
 
-    get('new session') do
-      response(200, 'successful') do
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
-
     post('create session') do
       consumes 'application/json'
       produces 'application/json'
